11a48ecfe93a8eaeec312e282a29ab25ba794fb4
Michael Stewart <tgm@vt.edu>
2014-10-15 22:57:43 -0400

    still isn't printing in the page?

--- result/index.php
+++ result/index.php
@@ -1,5 +1,6 @@
 <?php 
-  file_put_contents("udid-output.txt", "got to the result/index.php".$_GET["params"]); 
+  $udid = $_GET["params"];
+  file_put_contents("udid-output.txt", $udid); 
 ?>
 
 <html>
@@ -9,8 +10,9 @@
   <body>
     <h1>iOS UDID</h1>
     <div>
-      <?=$_GET["params"]?>
+      <?=$udid?>
     </div>
     <p>With many thanks to <a href="http://www.joshwright.com/tips/getting-an-iphone-udid-from-mobile-safari">Josh Wright</a>, <a href="http://stackoverflow.com/a/9426573/1449799">Daniel Brajkovic</a>, and <a href="http://stackoverflow.com/a/6442198/1449799">Kev Swindells(?)</a>. </p>
+    <?php echo $udid; ?>
   </body>
 </html>
