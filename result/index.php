e5aa63c9f241be55a5221c05cb8edb5cb624e7f1
Michael Stewart <tgm@vt.edu>
2014-10-15 23:10:58 -0400

    does the echo shortcut not work??

--- result/index.php
+++ result/index.php
@@ -14,7 +14,9 @@
   <body>
     <h1>iOS UDID</h1>
     <pre>
-      <?=$udid_val?>
+      <?php 
+        echo $udid_val;
+      ?>
     </pre>
     <p>With many thanks to <a href="http://www.joshwright.com/tips/getting-an-iphone-udid-from-mobile-safari">Josh Wright</a>, <a href="http://stackoverflow.com/a/9426573/1449799">Daniel Brajkovic</a>, and <a href="http://stackoverflow.com/a/6442198/1449799">Kev Swindells(?)</a>. </p>
   </body>
