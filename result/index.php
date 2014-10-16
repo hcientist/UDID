f3db77b90e4304c4a90775c07e5d64aabf4106eb
Michael Stewart <tgm@vt.edu>
2014-10-15 23:01:54 -0400

    try some decoding

--- result/index.php
+++ result/index.php
@@ -1,5 +1,6 @@
 <?php 
   $udid = $_GET["params"];
+  $decoded = rawurldecode($udid);
   file_put_contents("udid-output.txt", $udid); 
 ?>
 
@@ -14,5 +15,6 @@
     </div>
     <p>With many thanks to <a href="http://www.joshwright.com/tips/getting-an-iphone-udid-from-mobile-safari">Josh Wright</a>, <a href="http://stackoverflow.com/a/9426573/1449799">Daniel Brajkovic</a>, and <a href="http://stackoverflow.com/a/6442198/1449799">Kev Swindells(?)</a>. </p>
     <?php echo $udid; ?>
+    <?php echo $decoded; ?>
   </body>
 </html>
--- udid.php
+++ udid.php
@@ -2,6 +2,6 @@
   file_put_contents("udid-output.txt", "got to UDID/udid.php");
   $data = file_get_contents('php://input');
   $output = rawurlencode($data);
-  file_put_contents("udid-output-data.txt", $output);
+  file_put_contents("udid-output-data.txt", $data);
   header("Location: http://people.cs.vt.edu/tgm/old/UDID/result?params={$output}", true, 301);
 ?>
