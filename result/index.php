678b6917123bcb64faec029312c62e1be47db9f8
Michael Stewart <tgm@vt.edu>
2014-10-15 22:52:10 -0400

    is the pre messing it up?

--- result/index.php
+++ result/index.php
@@ -8,9 +8,9 @@
   </head>
   <body>
     <h1>iOS UDID</h1>
-    <pre>
+    <div>
       <?=$_GET["params"]?>
-    </pre>
+    </div>
     <p>With many thanks to <a href="http://www.joshwright.com/tips/getting-an-iphone-udid-from-mobile-safari">Josh Wright</a>, <a href="http://stackoverflow.com/a/9426573/1449799">Daniel Brajkovic</a>, and <a href="http://stackoverflow.com/a/6442198/1449799">Kev Swindells(?)</a>. </p>
   </body>
 </html>
